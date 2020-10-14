3e7fee1386624df905a63229343824e37558d205
gurudatta07 <gurudatta07@gmail.com>
2020-10-14 17:15:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 
 setup(
-    name='py_utils',
+    name='visionutils',
     version='1.0.0',
     packages=[],
-    url='git+https://github.com/gurudatta07/py_util.git',
+    url='git+https://github.com/gurudatta07/visionutils.git',
     license='MIT',
     author='Gurudatta',
     description='pytorch CNN models for visualization'
