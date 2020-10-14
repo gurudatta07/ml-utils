f92b3b86749190d1fca86eaa9e054c92c135bc55
gurudatta07 <gurudatta07@gmail.com>
2020-10-14 13:11:21 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='py utils',
+    version='1.0.0',
+    packages=[],
+    url='git+https://github.com/gurudatta07/py_util.git',
+    license='MIT',
+    author='Gurudatta',
+    description='pytorch CNN models for visualization'
+)
