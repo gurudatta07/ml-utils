093fcbf0f5ab165da69f35dd33c88e01743ed56d
gurudatta07 <gurudatta07@gmail.com>
2020-10-16 13:24:26 +0530

    Update train.py

--- utils/train.py
+++ utils/train.py
@@ -43,3 +43,32 @@ def train(model, device, train_loader, optimizer, epoch):
 
     pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
     train_acc.append(100*correct/processed)
+    
+    
+def train(model, device, trainloader, optimizer, epoch, criterion):
+    model.train()
+
+    pbar = tqdm(trainloader)
+    running_loss = 0.0
+    correct = 0
+    processed = 0
+
+    for idx, data in enumerate(pbar) : #trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+        
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        _, predicted = torch.max(outputs.data, 1)
+        processed += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+        pbar.set_description(desc= f'Loss={loss.item()} Batch_id={idx} Accuracy={100*correct/processed:0.2f}')
